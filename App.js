b86660811d5fa1bf41e2bf59278ea8f4f6032f53
Kam Kooner <85206896+KamKooner@users.noreply.github.com>
2022-06-30 11:10:47 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+/*
+Challenge: Build the Navbar component.
+Check the Figma file for the design specifics.
+*/
+
+export default function App() {
+    return (
+       <div>
+            <div className='nav-cont'>
+                <img src='./images/airbnb-logo.png' id='logo'></img>
+            </div> 
+       </div>
+    )
+}
